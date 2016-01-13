e4fa1104214dd2c4aa0d740268d3bcf4cdd1fba5
Glauber Gualda <Glauber Gualda>
2016-01-13 11:40:37 -0200

    aula 10

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playersTurn = true;
 
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 	
 	void Awake ()
     {
@@ -17,6 +21,7 @@ public class GameManager : MonoBehaviour {
         else if (instance != this)
             Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();	
 	}
@@ -30,8 +35,34 @@ public class GameManager : MonoBehaviour {
     {
         enabled = false;
     }
-	// Update is called once per frame
-	void Update () {
 	
+	void Update ()
+    {
+        if (playersTurn || enemiesMoving)
+            return;
+        StartCoroutine(MoveEnemies());	
 	}
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(turnDelay);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
+    }
 }
